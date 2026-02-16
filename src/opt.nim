a2a1d08ba74565ede1c2793ebad717590f3a7889
luthfi <luthlirik@gmail.com>
2026-02-16 19:03:42 +0700

    Refactoring between opt and questionable/option

--- src/opt.nim
+++ src/opt.nim
@@ -1,40 +1,68 @@
 import
-  tui/questionable/[option, base]
+  json, strutils
+
+type
+  OptionJson* = JsonNode
+
+proc putEnum*(plate: var OptionJson; val: openArray[string]; key: string): void {.inline.} =
+  plate[key] = %val
+
+proc put*(plate: var OptionJson; val, key: string) : void {.inline.} =
+  plate[key] = %val
+
+proc putRange*(plate: var OptionJson; sn, bp: int; key: string; default: int = sn) : void {.inline.} =
+  var s: seq[string]
+  
+  if default != sn:
+    s.add $default
+
+  for i in sn..bp:
+    s.add $i
+  plate[key] = %s
+
+proc s*(plate: OptionJson): string =
+  plate.getStr()
+
+proc n*(plate: OptionJson; default: int = 0) : int =
+  try:
+    plate.getStr().parseInt()
+  except:
+    default
 
 export
-  Questionable, OptionJson, OptionValuedQuestionable
+  OptionJson
 
 export  
-  ask, put, putEnum, putRange
+  put, putEnum, putRange
   
 when isMainModule:
-
   discard """
-    var opt: OptionJson = newJObject()
+  import tui/questionable/option
+  var opt: OptionJson = newJObject()
 
-    opt.put("default", "api")
-    opt.putEnum(["ffmpeg", "mpv", "vlc"], "player")
-    opt.putRange(1, 24, "fps")
-    
-    opt.ask()
+  opt.put("default", "api")
+  opt.putEnum(["ffmpeg", "mpv", "vlc"], "player")
+  opt.putRange(1, 24, "fps")
+  
+  opt.ask()
 
-    echo opt["api"].s
-    echo opt["player"].s
-    echo opt["fps"].n
+  echo opt["api"].s
+  echo opt["player"].s
+  echo opt["fps"].n
   """
 
   discard """
-    type Rijal = ref object of RootObj
-      nama: string
-      umur: int
-      opt: OptionJson = newJObject()
+  type Rijal = ref object of RootObj
+    nama: string
+    umur: int
+    opt: OptionJson = newJObject()
 
-    proc newRijal() : Rijal =
-      result = Rijal()
-      result.opt.put("-", "nama")
-      result.opt.putEnum(["On", "Off"], "status")
-      result.opt.putRange(0, 100, "hitam", 50)
+  proc newRijal() : Rijal =
+    result = Rijal()
+    result.opt.put("-", "nama")
+    result.opt.putEnum(["On", "Off"], "status")
+    result.opt.putRange(0, 100, "hitam", 50)
 
-    let rijal = newRijal()
-    rijal.opt.ask()
+  let rijal = newRijal()
+  rijal.opt.ask()
   """
--- src/tui/questionable/option.nim
+++ src/tui/questionable/option.nim
@@ -1,12 +1,11 @@
 import
-  sequtils, sugar, json, strutils
+  sequtils, json, strutils, sugar
 
 import
-  ./base, ../ask
+  ./base, ../ask,
+  ../../opt
 
 type
-  OptionJson* = JsonNode
-
   OptionValuedQuestionable* {.inheritable.} = ref object of Questionable
     value*: string
     key*: string
@@ -100,31 +99,6 @@ method handleExceptionKey*(currentItem: OptionStringQuestionable; tui: WewboTUI;
   else:
     currentItem.value &= ($key).toLowerAscii
 
-proc putEnum*(plate: var OptionJson; val: openArray[string]; key: string): void {.inline.} =
-  plate[key] = %val
-
-proc put*(plate: var OptionJson; val, key: string) : void {.inline.} =
-  plate[key] = %val
-
-proc putRange*(plate: var OptionJson; sn, bp: int; key: string; default: int = sn) : void {.inline.} =
-  var s: seq[string]
-  
-  if default != sn:
-    s.add $default
-
-  for i in sn..bp:
-    s.add $i
-  plate[key] = %s
-
-proc s*(plate: OptionJson): string =
-  plate.getStr()
-
-proc n*(plate: OptionJson; default: int = 0) : int =
-  try:
-    plate.getStr().parseInt()
-  except:
-    default
-
 proc ask*(plate: var OptionJson; title: string = "Select Option"): void =
   var cont: seq[OptionValuedQuestionable]
   
@@ -145,6 +119,3 @@ proc ask*(plate: var OptionJson; title: string = "Select Option"): void =
   
   for key in plate.keys:
     plate[key] = %cont.get(key)
-
-export
-  base, `[]`, newJObject
